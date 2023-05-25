8fdc8f907b345df5cc345ded41a36c1e34d31d56
Squezer123 <krzysztof.kordiukiewicz@gmail.com>
2023-05-25 12:58:50 +0200

    1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let tile = document.getElementsByClassName("tile");
-let box = document.getElementsByClassName("moving-block")
+let box = document.getElementsByClassName("moving-block");
+let width = tile[0].offsetWidth;
 box.addEventListener("load", szerokosc())
 function szerokosc(){
-    box.style.width = tile[0].offsetWidth;
+    box[0].style.width = tile[0].offsetWidth;
 }
