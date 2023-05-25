1f3c2487e17751ffaef5b363340df006b61f2645
Squezer123 <krzysztof.kordiukiewicz@gmail.com>
2023-05-25 21:55:53 +0200

    1

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let tile = document.getElementsByClassName("tile");
 let box = document.getElementsByClassName("moving-block");
 let width = tile[0].offsetWidth;
-box.addEventListener("load", szerokosc())
-function szerokosc(){
-    box[0].style.width = tile[0].offsetWidth;
-}
+/*box.addEventListener("load", alert("Portfolio jest przystosowane do oglądania na monitorze full hd (1920x1080)"));
