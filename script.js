b1d06445c3f77a548e4cdd6ec29f31a43e158629
Krzysztof Kordiukiewiczi <krzysztof.kordiukiewicz@gmail.com>
2023-05-27 22:34:55 +0200

    1

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let tilewidth = document.getElementsByClassName('col')[0].offsetWidth;
+let tileheight = document.getElementsByClassName('col')[0].offsetHeight;
 let movingbox = document.getElementsByClassName('moving-block');
 
 movingbox[0].addEventListener("load", ustawienie());
 movingbox[0].addEventListener("resize", ustawienie());
 function ustawienie(){
     movingbox[0].style.width = tilewidth;
+    movingbox[0].style.height = tileheight;
 }
 
 
